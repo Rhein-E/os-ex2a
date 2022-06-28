15db2cc4518a3893c0875f65dff147ca607b4a7a
Rhein_E <1308498716@qq.com>
2022-06-28 11:07:34 +0800

    elf32 加载需要的结构体

---
+++ linux/include/elf32.h
@@ -0,0 +1,32 @@
+#ifndef ELF32_H
+#define ELF32_H
+
+typedef struct {
+    unsigned char magic[16];
+    unsigned short type;
+    unsigned short machine;
+    unsigned long version;
+    unsigned long entry;
+    unsigned long phdr_off;
+    unsigned long shdr_off;
+    unsigned long flags;
+    unsigned short ehdr_size;
+    unsigned short phdr_size;
+    unsigned short phdr_num;
+    unsigned short shdr_size;
+    unsigned short shdr_num;
+    unsigned short stridx;
+} elf32_ehdr;
+
+typedef struct {
+    unsigned long type;
+    unsigned long offset;
+    unsigned long vaddr;
+    unsigned long paddr;
+    unsigned long filesize;
+    unsigned long memsize;
+    unsigned long flags;
+    unsigned long align;
+} elf32_phdr;
+
+#endif // ELF32_H
